3e374446a067db0753acfe3747d6b6d683dc19d4
plato-cambrian <plato@cambrian.org>
2014-09-24 15:25:18 -0600

    WIP IGB panics. Having some network connectivity issues with the server talking to pubnub.

--- server.js
+++ server.js
@@ -1,69 +1,75 @@
-//app.js
+//server.js
 
+// Configure Pubnub:
+var PUBNUB = require('pubnub');
+
+// Load the server-pubnub.js file, it returns a function
+// Immediately invoke this function, passing in the PUBNUB object
+// Set the variable pubnub to the result of this function call
+var panicPubnub = 
+(require('./lib/server-pubnub.js'))(PUBNUB);
+//var pubnub = panicPubnub.pubnub;
+
+var panicQueue = 
+(require('./lib/panic-queue.js'))(panicPubnub);
+
+// Configure Express server:
 var express = require('express');
 var path = require('path');
 
 var app = express();
+// Configure jade templates and directory:
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'jade');
+
+// Configure sessions:
 app.use(express.cookieParser());
 app.use(express.session({secret:'my super secret secretapapppxxllerjam,dfa654163210321'}));
+
+// Parse form posts:
 app.use(express.bodyParser());
-app.use(express.logger('dev'));
+
+// Host every static file in the static/ directory
 app.use(express.static(path.join(__dirname, 'static')));
 
+// Try and match requests to the routes defined in routes.js:
 app.use(app.router);
 
-app.all('*', function(req, res, next){
-  //console.log('Headers for', req.path, '\n', JSON.stringify(req.headers, null, 2));
-  res.locals.pilot = {};
-  var isTrusted = 
-    req.headers.eve_trusted === 'Yes' 
-      ? true : false;
-  if(isTrusted){
-    console.log('we have trust');
-    res.locals.pilot.name = req.headers.eve_charname;
-    res.locals.pilot.system = req.headers.eve_solarsystemname;
-  }
-  res.locals.isIGB = 
-    req.headers.eve_trusted != "" 
-    ? true : false;
+// Log requests to console
+app.use(express.logger('dev'));
 
-  req.session.isTrusted = isTrusted;
-  res.locals.isTrusted = isTrusted;
+// Set up routing
+// The other file is out of scope. It returns a function, we call this function, passing dependencies
+// The result of the function call is, all the routes are now bound to app
+(require('./lib/routes.js'))(app, panicPubnub, panicQueue); 
 
-  // Initialize this to false if it's undefined:
-  req.session.panicReceived = 
-    req.session.panicReceived === undefined
-      ? false : req.session.panicReceived;
-  next();
-});
+// Wait to fire up the server until we have our Pubnub channels connected:
+function waitTillPubNubOrTimeout(){
+  var timedout = false;
+  var panicOK = false;
+  var backupOK = false;
+  panicPubnub.events.on('connected', function(channel){
+    if(channel === 'panic'){
+      panicOK = true;
+    } else if(channel === 'backup'){
+      backupOK = true;
+    }
+    if(panicOK && backupOK){
+      startServer();
+    }
+  })
+  
+  setTimeout(function(){
+    if(!panicOK || !backupOK){
+      throw('Waited 30 seconds with no pubnub connection')
+    }
+  }, 30*1000); // 30 seconds
+}
 
-app.get('/', function(req, res){
-  // Copy the flag that a panic was received or not into locals
-  if(req.session.panicReceived){
-    console.log('Panicking...')
-  }
-  res.locals.panicReceived = req.session.panicReceived;
-  // Clear the flag
-  req.session.panicReceived = false;
-  res.render('panic');
-});
+function startServer(){
+  require('http').createServer(app).listen(3000, function(err){
+    console.log(err ? err : "Listening on 3000");
+  })
+}
 
-app.get('/panic', function(req, res){
-  var out = 
-  'PANIC!!!' + '\n'
-  + 'Headers ' + JSON.stringify(req.headers, null, 2);
-  console.log(out);
-  req.session.panicReceived = true;
-  res.redirect('/');
-});
-
-app.get('/standby', function(req, res){
-  res.render('standby');
-});
-
-// Fire up the server:
-require('http').createServer(app).listen(3000, function(err){
-  console.log(err ? err : "Listening on 3000");
-})
+waitTillPubNubOrTimeout();
